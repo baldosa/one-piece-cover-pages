6799d66eaa93a8071305c781612c22c9cfc0d7f5
Ema <mast3er@gmail.com>
2021-11-25 21:03:06 -0300

    working version

--- one_piece.py
+++ one_piece.py
@@ -1,113 +1,50 @@
+#%%
 """
-Downloads One Piece color spreads and covers from One Piece Wiki in fandom.com
+Downloads One Piece color spreads and covers from One Piece Wiki at fandom.com
 """
-
-
-import itertools
-import json
 import requests
-import urllib.request
 from bs4 import BeautifulSoup
-import signal
-import sys
+from pysondb import db
+from one_piece_color.gphotos import upload_photos, auth
+from one_piece_color.download import download_file
+
+ALBUM_ID = 'AMQn-3ZPXaYZq6laEwY7KfBpCV9BzEVr7cVEm0lammX_i5cZucjZVsTgbuPWm9CIHgjQOT3aQP3O'
+DB_FILE = 'images.json'
+
+URLS = [
+    [ 'https://onepiece.fandom.com/wiki/Category:Color_Spreads', 'Color Spreads' ],
+    [ 'https://onepiece.fandom.com/wiki/Category:Color_Covers', 'Color Covers' ]
+]
+
+CLASS_NAME = 'category-page__member-thumbnail'
+
+def main(url, color_type, img_db, service):
+    webpage = requests.get(url).text
+
+    soup = BeautifulSoup(webpage, 'html.parser')
+
+    img_list = soup.findAll('img', {'class': CLASS_NAME})
+
+    for img in img_list:
+        print(img['alt'])
+        data = {
+            'src': img['src'][0:img['src'].find('latest')+6],
+            'chapter': img['alt'].replace('.png', ''),
+            'filename': img['alt'],
+            'type': color_type
+        }
+        if not img_db.getByQuery({'filename': data['filename']}):
+            print('uploading')
+            download_file(data['src'], data['filename'])
+            upload_photos(service, f"images/{data['filename']}", ALBUM_ID)
+            img_db.add(data)
 
 
-URLS = ['https://onepiece.fandom.com/wiki/Category:Color_Spreads',
-        'https://onepiece.fandom.com/wiki/Category:Color_Covers']
-IMG_CLASS = 'category-page__member-thumbnail'
-
-
-def load_db():
-    """
-    Load images.json where we store already downloaded images.
-    """
-    print('reading json')
-    try:
-        with open("images.json", "rb") as json_file:
-            data_imgs = json.load(json_file)
-            print(f'{len(data_imgs)} images in db')
-            return data_imgs
-    except IOError:
-        print('Nothing in db')
-        return []
-
-
-def save_db(image_list):
-    """
-    Saves downloaded images to images.json
-    """
-    print('saving json')
-    with open('images.json', 'w') as j:
-        json.dump(image_list, j, indent=4, sort_keys=True)
-
-
-def list_images(url):
-    print('listing imgs')
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    links = soup.find_all(class_=IMG_CLASS)
-
-    imgs = []
-    for link in links:
-        if 'data:image' not in link['src']:
-            imgs.append({
-                'src': link['src'],
-                'title': link['alt'],
-                'url': url
-            })
-    return imgs
-
-
-def download_img(entry):
-    download_link = entry['src'][0:entry['src'].find('latest')+7]
-    title = entry['src'][entry['src'].find(
-        '/Chapter'):entry['src'].find('/revision')]
-    urllib.request.urlretrieve(download_link, f'imgs//{title}')
-    # return title
-
-# def upload_imgur(file):
-#     download_link = entry['src'][0:entry['src'].find('latest')+7]
-#     title = entry['src'][entry['src'].find(
-#         '/Chapter'):entry['src'].find('/revision')]
-#     urllib.request.urlretrieve(download_link, f'imgs//{title}')
-
-#     return title
-
-def exit_gracefully(signum, frame):
-    # restore the original signal handler as otherwise evil things will happen
-    # in raw_input when CTRL+C is pressed, and our signal handler is not re-entrant
-    signal.signal(signal.SIGINT, original_sigint)
-
-    print("Ok ok, quitting")
-    save_db(ALREADY_saved)
-    sys.exit(1)
-
-    # restore the exit gracefully handler here    
-    signal.signal(signal.SIGINT, exit_gracefully)
-
-# %%
 if __name__ == '__main__':
-    # original_sigint = signal.getsignal(signal.SIGINT)
-    # signal.signal(signal.SIGINT, exit_gracefully)
-    ALREADY_saved = load_db()
-    for URL in URLS:
-        images = list_images(URL)
-        new_images = list(itertools.filterfalse(lambda x: x in images, ALREADY_saved)) + \
-            list(itertools.filterfalse(lambda x: x in ALREADY_saved, images))
-        if len(new_images)>0:
-            print(f'downloading {len(new_images)} files')
-            ALREADY_saved += new_images
-            for img in new_images:
-                try:
-                    download_img(img)
-                except:
-                    print('error')
-                    print(img)
-                    pass
-        
-        # upload_imgur(file)
+    img_db = db.getDb(DB_FILE)
+    service, creds = auth()
 
-    save_db(ALREADY_saved)
-    print('finished')
+    for url in URLS:
+        main(url[0], url[1], img_db, service)
 
 # %%
---
+++ one_piece_color/gphotos.py
@@ -0,0 +1,93 @@
+# striped down from https://github.com/eshmu/gphotos-upload
+
+from __future__ import print_function
+import json
+import os.path
+from google.auth.transport.requests import AuthorizedSession
+import os.path
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google.auth.transport.requests import AuthorizedSession
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/photoslibrary',
+          'https://www.googleapis.com/auth/photoslibrary.appendonly',
+          'https://www.googleapis.com/auth/photoslibrary.sharing']
+
+
+def auth():
+    """Shows basic usage of the Drive v3 API.
+    Prints the names and ids of the first 10 files the user has access to.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'client_id.json', SCOPES)
+            creds = flow.run_console(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    session = AuthorizedSession(creds)
+    return session, creds
+
+
+def upload_photos(session, photo_file_name, album_id):
+
+    session.headers["Content-type"] = "application/octet-stream"
+    session.headers["X-Goog-Upload-Protocol"] = "raw"
+
+    try:
+        with open(photo_file_name, mode='rb') as photo_file:
+            photo_bytes = photo_file.read()
+
+            session.headers["X-Goog-Upload-File-Name"] = os.path.basename(
+                photo_file_name)
+
+            upload_token = session.post(
+                'https://photoslibrary.googleapis.com/v1/uploads', photo_bytes)
+
+            if (upload_token.status_code == 200) and (upload_token.content):
+
+                create_body = json.dumps({"albumId": album_id, "newMediaItems": [
+                                         {"description": "", "simpleMediaItem": {"uploadToken": upload_token.content.decode()}}]}, indent=4)
+
+                resp = session.post(
+                    'https://photoslibrary.googleapis.com/v1/mediaItems:batchCreate', create_body).json()
+
+                # print("Server response: {}".format(resp))
+
+                if "newMediaItemResults" in resp:
+                    status = resp["newMediaItemResults"][0]["status"]
+                    if status.get("code") and (status.get("code") > 0):
+                        print("Could not add \'{0}\' to library -- {1}".format(
+                            os.path.basename(photo_file_name), status["message"]))
+                    else:
+                        print(f"Uploaded {os.path.basename(photo_file_name)}")
+                else:
+                    print("Could not add \'{0}\' to library. Server Response -- {1}".format(
+                        os.path.basename(photo_file_name), resp))
+
+            else:
+                print("Could not upload \'{0}\'. Server Response - {1}".format(
+                    os.path.basename(photo_file_name), upload_token))
+
+    except OSError as err:
+        print('error')
+
+    try:
+        del(session.headers["Content-type"])
+        del(session.headers["X-Goog-Upload-Protocol"])
+        del(session.headers["X-Goog-Upload-File-Name"])
+    except KeyError:
+        pass
